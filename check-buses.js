bb744c0045ffca15d60a6f22c7be1b6b67bed02a
TTony <thriev.andreiudrea@gmail.com>
2026-09-21 19:00:06 +0300

    Update check-buses.js

--- check-buses.js
+++ check-buses.js
@@ -15,7 +15,11 @@ const API_URLS = [
 
 // Defined as "operator:vehicleId" or license plate string
 const RARE_VEHICLES = new Set([
-  "transmixt:41,53,62,122,133,145"
+  "transmixt:41",
+  "transmixt:53",
+  "transmixt:62",
+  "transmixt:133",
+  "transmixt:145"
 ]);
 
 // Optionally restrict rare bus alerts to specific routes (leave [] for all routes)
